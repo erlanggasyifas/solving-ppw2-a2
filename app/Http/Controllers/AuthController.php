209c9d7325e563d280ea3b42648d3fb9437dcfef
ergegananputra <ergegananputra@gmail.com>
2025-10-23 10:22:50 +0700

    pertemuan 8

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function showLoginForm() {
+        return view('auth.login');
+    }
+
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email atau password salah.',
+        ]);
+
+
+    }
+
+    public function showRegisterForm() {
+        return view('auth.register');
+    }
+
+    public function register(Request $request) {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $user = \App\Models\User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('login')->with(
+            'success', 'Registration successful. Please login.'
+        );
+
+    }
+
+    public function logout() {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+}
